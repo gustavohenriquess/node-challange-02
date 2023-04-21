06c085cb1e1c07049208848e81d3fd8d2ea09216
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-04-21 16:28:20 -0300

    test: added test e2e for user

---
+++ test/e2e/user.spec.ts
@@ -0,0 +1,59 @@
+import {
+  it,
+  beforeAll,
+  afterAll,
+  describe,
+  beforeEach,
+  afterEach,
+} from 'vitest'
+// import { execSync } from 'node:child_process'
+import request from 'supertest'
+import { app } from '../../src/app'
+
+describe('User routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(async () => {
+    // execSync('npm run knex migrate:latest')
+  })
+
+  afterEach(async () => {
+    // execSync('npm run knex migrate:rollback --all')
+  })
+
+  describe('Success', () => {
+    it('should be able to create a user', async () => {
+      await request(app.server)
+        .post('/user')
+        .send({
+          user: 'Gustavo Henrique',
+          password: 'PasSwOrd',
+        })
+        .expect(201)
+    })
+
+    it('should be able to login with a user', async () => {
+      const user = 'Gustavo_Henrique'
+      const password = 'PasSwOrd'
+
+      await request(app.server)
+        .post('/user')
+        .send({
+          user,
+          password,
+        })
+        .expect(201)
+
+      await request(app.server)
+        .post('/user/login')
+        .auth(user, password)
+        .expect(200)
+    })
+  })
+})
