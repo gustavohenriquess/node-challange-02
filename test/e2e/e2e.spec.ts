d7b1410d977bbe42146df05ba622c6babc1b53a9
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-04-23 14:33:02 -0300

    test: merged test in one file

--- test/e2e/e2e.spec.ts
+++ test/e2e/e2e.spec.ts
@@ -11,28 +11,89 @@ import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../../src/app'
 
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+describe('User routes', () => {
+  const user = 'Gustavo_Henrique'
+  const password = 'PasSwOrd'
+
+  beforeEach(async () => {
+    execSync('npm run knex migrate:latest')
+  })
+
+  afterEach(async () => {
+    execSync('npm run knex migrate:rollback --all')
+  })
+
+  describe('Success', () => {
+    it('should be able to create a user', async () => {
+      await request(app.server)
+        .post('/user')
+        .send({
+          user,
+          password,
+        })
+        .expect(201)
+    })
+
+    it('should be able to login with a user', async () => {
+      await request(app.server)
+        .post('/user')
+        .send({
+          user,
+          password,
+        })
+        .expect(201)
+
+      await request(app.server)
+        .post('/user/login')
+        .auth(user, password)
+        .expect(200)
+    })
+  })
+
+  describe('Fail', () => {
+    it('shouldn`t be able to create a user', async () => {
+      await request(app.server)
+        .post('/user')
+        .send({
+          user: 'Gustavo Henrique',
+        })
+        .expect(500)
+    })
+
+    it('shouldn`t be able to login with a user', async () => {
+      const loginResponse = await request(app.server)
+        .post('/user/login')
+        .auth(user, password)
+        .expect(401)
+
+      expect(loginResponse.body).toEqual(
+        expect.objectContaining({ error: 401, message: 'Invalid credentials' }),
+      )
+    })
+  })
+})
+
 describe('Meals routes', () => {
   const user = 'Gustavo_Henrique'
   const password = 'PasSwOrd'
   let cookies: string
 
-  beforeAll(async () => {
-    await app.ready()
-  })
-
-  afterAll(async () => {
-    await app.close()
-  })
-
   beforeEach(async () => {
     execSync('npm run knex migrate:latest')
-    await request(app.server)
-      .post('/user')
-      .send({
-        user,
-        password,
-      })
-      .expect(201)
+    console.log('aqui', user, password)
+    const usuario = await request(app.server).post('/user').send({
+      user,
+      password,
+    })
+    console.log(usuario.body)
 
     const userLoginResponse = await request(app.server)
       .post('/user/login')
